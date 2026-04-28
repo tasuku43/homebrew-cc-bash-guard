9dfe9e2c42a570302b495fef1bc9117c92cdd397
tasuku43 <43329036+tasuku43@users.noreply.github.com>
2026-04-28 13:40:57 +0000

    Update Homebrew formula for v0.1.4

--- Formula/cc-bash-guard.rb
+++ Formula/cc-bash-guard.rb
@@ -3,25 +3,25 @@ class CcBashGuard < Formula
   homepage "https://github.com/tasuku43/cc-bash-guard"
   license "MIT"
 
-  version "0.1.2"
+  version "0.1.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.1.2/cc-bash-guard_v0.1.2_macos_arm64.tar.gz"
-      sha256 "76f5f668320afc2378f9799f16a7d934e20bc49ceae2f7ab8de69389aed9cea8"
+      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.1.4/cc-bash-guard_v0.1.4_macos_arm64.tar.gz"
+      sha256 "8a1efa3bbecf175bc1a592557e60953b698b8fb79a35174e7ac6cc129a1c35f4"
     else
-      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.1.2/cc-bash-guard_v0.1.2_macos_x64.tar.gz"
-      sha256 "725743982a81240fb580602fe8eea8c0032bbb3f3664a30a6a587d3358a73c06"
+      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.1.4/cc-bash-guard_v0.1.4_macos_x64.tar.gz"
+      sha256 "70e94eb8016371a152c15ac7e3454fed66a5b97e8577b1c54a3640d923c23024"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.1.2/cc-bash-guard_v0.1.2_linux_arm64.tar.gz"
-      sha256 "3c7eb097a934513357e2f7ab003c8b2e8caa2b83da3997f67ee9b8c015b9e4f7"
+      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.1.4/cc-bash-guard_v0.1.4_linux_arm64.tar.gz"
+      sha256 "290e236903ebdbb9d128b3aedb921d94ff1fc33ceb006b3ebfaf2a4d0ddc7f35"
     else
-      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.1.2/cc-bash-guard_v0.1.2_linux_x64.tar.gz"
-      sha256 "25ddbbc8c0fec695b26c52cbd8218999f6a507e141aa91995eb8522674a89289"
+      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.1.4/cc-bash-guard_v0.1.4_linux_x64.tar.gz"
+      sha256 "a5a902a47eaaf7696469da96835ac798bff7d472148b610fa6725bec43cfc166"
     end
   end
 
